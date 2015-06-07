3dfaeda74aeea7d14cf90fad3beb1632c77e177c
Integer64 <tivan@bk.ru>
2015-06-07 22:04:59 +0800

    Добавлены комментарии

--- vecktor_OOP/models/Vecktor.php
+++ vecktor_OOP/models/Vecktor.php
@@ -1,19 +1,29 @@
 <?php
 
 class Vecktor {
+
+    // Департаменты
     public $departmentOfPurchases;
     public $salesDepartment;
     public $departmentOfAdvertising;
     public $departmentOfLogistics;
+
+    // Массив для департаментов
+    // нужен для более простого заполнения
     private $allDepartments = [];
 
+    // Конструктор.
+    // Заполняем все департаменты работниками
     public function __construct()
     {
+        // Создаем департаменты
         $this->departmentOfPurchases = new DepartmentOfPurchases();
         $this->salesDepartment = new SalesDepartment();
         $this->departmentOfAdvertising = new DepartmentOfAdvertising();
         $this->departmentOfLogistics = new DepartmentOfLogistics();
 
+        // Массив сотрудников.
+        // Для департамента закупок
         $employeesDepartmentOfPurchases = [
             array(9 => new Manager(1, false)),
             array(3 => new Manager(2, false)),
@@ -21,8 +31,14 @@ class Vecktor {
             array(2 => new Marketer(1, false)),
             array(1 => new Manager(2, true))
         ];
+
+        // Вызываем метод для заполнения департамента
+        // 1 параметр - департамент
+        // 2 параметр - массив с сотрудниками
         $this->fillTheDepartment($this->departmentOfPurchases, $employeesDepartmentOfPurchases);
 
+        // Массив сотрудников.
+        // Для департамента продаж
         $employeesSalesDepartment = [
             array(12 => new Manager(1, false)),
             array(6 => new Marketer(1, false)),
@@ -30,8 +46,14 @@ class Vecktor {
             array(2 => new Analyst(2, false)),
             array(1 => new Marketer(2, true))
         ];
+
+        // Вызываем метод для заполнения департамента
+        // 1 параметр - департамент
+        // 2 параметр - массив с сотрудниками
         $this->fillTheDepartment($this->salesDepartment, $employeesSalesDepartment);
 
+        // Массив сотрудников.
+        // Для департамента рекламы
         $employeesDepartmentOfAdvertising = [
             array(15 => new Marketer(1, false)),
             array(10 => new Marketer(2, false)),
@@ -40,8 +62,13 @@ class Vecktor {
             array(1 => new Marketer(3, true)),
         ];
 
+        // Вызываем метод для заполнения департамента
+        // 1 параметр - департамент
+        // 2 параметр - массив с сотрудниками
         $this->fillTheDepartment($this->departmentOfAdvertising, $employeesDepartmentOfAdvertising);
 
+        // Массив сотрудников.
+        // Для департамента логистики
         $employeesDepartmentOfLogistics = [
             array(13 => new Manager(1, false)),
             array(5 => new Manager(2, false)),
@@ -49,14 +76,21 @@ class Vecktor {
             array(1 => new Manager(1, true)),
         ];
 
+        // Вызываем метод для заполнения департамента
+        // 1 параметр - департамент
+        // 2 параметр - массив с сотрудниками
         $this->fillTheDepartment($this->departmentOfLogistics, $employeesDepartmentOfLogistics);
 
+        // Заносим все департаменты в массив
         $this->allDepartments[] = $this->departmentOfPurchases;
         $this->allDepartments[] = $this->salesDepartment;
         $this->allDepartments[] = $this->departmentOfAdvertising;
         $this->allDepartments[] = $this->departmentOfLogistics;
     }
 
+    // Метод для заполнения департаментов.
+    // 1 параметр - департамент
+    // 2 параметр - массив с сотрудниками
     private function fillTheDepartment($department,$addToDepartment)
     {
         foreach($addToDepartment as $item)
@@ -71,6 +105,7 @@ class Vecktor {
         }
     }
 
+    // Метод вывода таблицы с статистикой
     public function printStatistic(){
         // Ширина колонок
         $col1 = 15;
@@ -89,20 +124,17 @@ class Vecktor {
             $this->padLeft("тугр./стр.", $col6) . "\n";
         echo "----------------------------------------------------------------------------" . "\n";
 
-        // Сама таблица
+        // Переменные для подсчета строки "Всего"
         $totalEmployees = 0;
         $totalWageCosts = 0;
         $totalCoffeeConsumption = 0;
         $totalPaperConsumption = 0;
         $totalAverageDischarge = 0;
 
-        $averageEmployees = 0;
-        $averageWageCosts = 0;
-        $averageCoffeeConsumption = 0;
-        $averagePaperConsumption = 0;
-        $averageAverageDischarge = 0;
-
+        // Сама таблица
         foreach ($this->allDepartments as $department) {
+
+            // Вывод данных в строку
             echo $this->padRight($department->getTitle(), $col1) .
                 $this->padLeft($department->getCountEmployees(), $col2) .
                 $this->padLeft($department->getWageCosts(),$col3).
@@ -110,6 +142,7 @@ class Vecktor {
                 $this->padLeft($department->getPaperConsumption(), $col5) .
                 $this->padLeft($department->getAverageDischarge(), $col6) . "\n";
 
+            // Суммируем данные и записываем в переменные
             $totalEmployees += $department->getCountEmployees();
             $totalWageCosts += $department->getWageCosts();
             $totalCoffeeConsumption += $department->getCoffeeConsumption();
@@ -117,7 +150,24 @@ class Vecktor {
             $totalAverageDischarge += $department->getAverageDischarge();
         }
 
+        // Переменные для подсчета строки "Среднее"
+        $averageEmployees = $totalEmployees / 4;
+        $averageWageCosts = $totalWageCosts / 4;
+        $averageCoffeeConsumption = $totalCoffeeConsumption / 4;
+        $averagePaperConsumption = $totalPaperConsumption / 4;
+        $averageAverageDischarge = $totalAverageDischarge / 4;
+
+
         // Конец таблица
+        // Вывод "Среднее"
+        echo $this->padRight("Среднее", $col1) .
+            $this->padLeft($averageEmployees, $col2) .
+            $this->padLeft($averageWageCosts,$col3).
+            $this->padLeft($averageCoffeeConsumption, $col4) .
+            $this->padLeft($averagePaperConsumption, $col5) .
+            $this->padLeft($averageAverageDischarge, $col6) . "\n";
+
+        // Вывод "Всего"
         echo $this->padRight("Всего", $col1) .
             $this->padLeft($totalEmployees, $col2) .
             $this->padLeft($totalWageCosts,$col3).
@@ -126,9 +176,11 @@ class Vecktor {
             $this->padLeft($totalAverageDischarge, $col6) . "\n";
     }
 
+    // Метод для вывода данных с смещением вправо
     private function padRight($string,$length){
         $space="";
 
+        // Вычитаем длину строки из колонки
         $length=$length-mb_strlen($string);
         for($i = 0;$i < $length; $i++){
             $space = $space." " ;
@@ -136,6 +188,7 @@ class Vecktor {
         return $string.$space;
     }
 
+    // Метод для вывода данных с смещением влево
     private function padLeft($string,$length){
         $space="";
 

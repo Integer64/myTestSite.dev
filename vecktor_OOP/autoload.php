0677f2b15bb4cc04080122af3ea3a3a550017108
Integer64 <tivan@bk.ru>
2015-06-08 22:33:51 +0800

    Переделал на SPL.

--- vecktor_OOP/autoload.php
+++ vecktor_OOP/autoload.php
@@ -1,10 +1,13 @@
 <?php
 
 // Функция для автоматического подключения классов и моделей
-function __autoload($class){
+function loadClass($class){
     if(file_exists(__DIR__ . '/models/' . $class . '.php')){
         require __DIR__ . '/models/' . $class . '.php';
     }else if(file_exists(__DIR__ . '/classes/' . $class . '.php')){
         require __DIR__ . '/classes/' . $class . '.php';
     }
 }
+
+spl_autoload_extensions('.php');
+spl_autoload_register('loadClass');

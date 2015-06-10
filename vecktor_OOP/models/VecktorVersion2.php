2862fe087e060d488aff6817a25d742f6ced8c59
Integer64 <tivan@bk.ru>
2015-06-10 13:25:11 +0800

    2 сценарий

---
+++ vecktor_OOP/models/VecktorVersion2.php
@@ -0,0 +1,67 @@
+<?php
+
+class VecktorVersion2 extends Vecktor{
+
+    public function __construct()
+    {
+        // Вызов родительского конструктора
+        parent::__construct();
+
+        // Проход по всем департаментам
+        foreach($this->allDepartments as $department){
+
+            // Получаем список работников в департаменте
+            $employees = $department->getEmployees();
+
+            // Запускаем процесс увелечения ЗП и кофе
+            $this->upgradeAnalysts($employees, $department);
+        }
+    }
+
+    private function upgradeAnalysts(SplObjectStorage $employees, Department $department)
+    {
+        // Шаблон самого умного Аналитика в департаменте
+        $greatestAnalyst = new Analyst(1, false);
+
+        // Делаем сериализацию в строку
+        $storageString = $employees->serialize();
+
+        // Проверем есть ли в департаменте Аналитики
+        if(strpos($storageString, 'Analyst') !== false) {
+
+            // Проходимся по стороджу с работниками
+            foreach ($employees as $employee) {
+
+                // Если работник руководитель и не является аналитиоком
+                if ($employee->getBoss() === true && !is_a($employee, 'Analyst')) {
+                    // Понижаем до простого работника
+                    $employee->setBoss(false);
+                }
+
+                // Если работник класса Аналитик
+                if (is_a($employee, 'Analyst')) {
+
+                    // Подымаем зп
+                    $employee->setPayment(1100);
+
+                    // Увеличиваем потребление кофе
+                    $employee->setCoffee(75);
+
+                    // Если этот аналитик круче нашего шаблона
+                    if ($employee->getRank() > $greatestAnalyst->getRank()) {
+
+                        // Понижаем старого крутого аналитика
+                        $greatestAnalyst->setBoss(false);
+
+                        // Назначаем нового крутым аналитиком
+                        $greatestAnalyst = $employee;
+
+                        // Ставим его начальником
+                        $greatestAnalyst->setBoss(true);
+                    }
+                }
+            }
+        }
+    }
+
+} 

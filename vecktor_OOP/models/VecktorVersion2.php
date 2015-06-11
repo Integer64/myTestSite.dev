fc4220d462ef97d61730539c2e6e6850bfa1cd77
Integer64 <tivan@bk.ru>
2015-06-11 22:48:23 +0800

    Переделал.

--- vecktor_OOP/models/VecktorVersion2.php
+++ vecktor_OOP/models/VecktorVersion2.php
@@ -14,51 +14,45 @@ class VecktorVersion2 extends Vecktor{
             $employees = $department->getEmployees();
 
             // Запускаем процесс увелечения ЗП и кофе
-            $this->upgradeAnalysts($employees, $department);
+            $this->upgradeAnalysts($employees);
         }
     }
 
-    private function upgradeAnalysts(SplObjectStorage $employees, Department $department)
+    // Метод повышение ЗП и кофе работникам
+    private function upgradeAnalysts(SplObjectStorage $employees)
     {
         // Шаблон самого умного Аналитика в департаменте
         $greatestAnalyst = new Analyst(1, false);
 
-        // Делаем сериализацию в строку
-        $storageString = $employees->serialize();
+        // Проходимся по стороджу с работниками
+        foreach ($employees as $employee)
+        {
+        // Если работник руководитель и не является аналитиоком
+            if ($employee->getBoss() && !$employee instanceof Analyst)
+            {
+                // Понижаем до простого работника
+                $employee->setBoss(false);
+            }
+            // Если работник класса Аналитик
+            if ($employee instanceof Analyst)
+            {
+                // Подымаем зп
+                $employee->setPayment(1100);
 
-        // Проверем есть ли в департаменте Аналитики
-        if(strpos($storageString, 'Analyst') !== false) {
+                // Увеличиваем потребление кофе
+                $employee->setCoffee(75);
 
-            // Проходимся по стороджу с работниками
-            foreach ($employees as $employee) {
+                // Если этот аналитик круче нашего шаблона
+                if ($employee->getRank() > $greatestAnalyst->getRank())
+                {
+                    // Понижаем старого крутого аналитика
+                    $greatestAnalyst->setBoss(false);
 
-                // Если работник руководитель и не является аналитиоком
-                if ($employee->getBoss() === true && !is_a($employee, 'Analyst')) {
-                    // Понижаем до простого работника
-                    $employee->setBoss(false);
-                }
+                    // Назначаем нового крутым аналитиком
+                    $greatestAnalyst = $employee;
 
-                // Если работник класса Аналитик
-                if (is_a($employee, 'Analyst')) {
-
-                    // Подымаем зп
-                    $employee->setPayment(1100);
-
-                    // Увеличиваем потребление кофе
-                    $employee->setCoffee(75);
-
-                    // Если этот аналитик круче нашего шаблона
-                    if ($employee->getRank() > $greatestAnalyst->getRank()) {
-
-                        // Понижаем старого крутого аналитика
-                        $greatestAnalyst->setBoss(false);
-
-                        // Назначаем нового крутым аналитиком
-                        $greatestAnalyst = $employee;
-
-                        // Ставим его начальником
-                        $greatestAnalyst->setBoss(true);
-                    }
+                    // Ставим его начальником
+                    $greatestAnalyst->setBoss(true);
                 }
             }
         }

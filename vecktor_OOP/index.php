f62d860fb27c77dc48f75b79f3e34d2cb1368613
Integer64 <tivan@bk.ru>
2015-06-13 15:13:42 +0800

    Переделал на АнтикризисМенеджмент класс.

--- vecktor_OOP/index.php
+++ vecktor_OOP/index.php
@@ -11,17 +11,7 @@ $vector = new Vecktor();
 // Вызываем у объекта метод вывода статиски на экран
 $vector->printStatistic();
 
-echo "\n";
-
-$vector1 = new VecktorVersion1();
-$vector1->printStatistic();
-
-echo "\n";
-
-$vector2 = new VecktorVersion2();
-$vector2->printStatistic();
-
-echo "\n";
-
-$vector3 = new VecktorVersion3();
-$vector3->printStatistic();
+$anticrisis = new AnticrisisManagement();
+$anticrisis->applyPlan1(new Vecktor());
+$anticrisis->applyPlan2(new Vecktor());
+$anticrisis->applyPlan3(new Vecktor());

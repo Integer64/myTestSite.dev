5227b07893a5bae1ccef57ad12f1d72d3c05bec4
Integer64 <tivan@bk.ru>
2015-06-23 21:00:12 +0800

    Функция автозагрузки классов

---
+++ CatMouse/autoload.php
@@ -0,0 +1,15 @@
+<?php
+
+// Функция для автоматического подключения классов и моделей
+function load($class){
+    $classParts = explode('\\', $class);
+    $classParts[0] = __DIR__;
+    unset($classParts[1]);
+    $path = implode(DIRECTORY_SEPARATOR, $classParts) . '.php';
+    if (file_exists($path)){
+        require $path;
+    }
+}
+
+spl_autoload_extensions('.php');
+spl_autoload_register('load');

d67679d1cd4b972d74a7fdaeb5f769621745d32d
Integer64 <tivan@bk.ru>
2015-06-23 21:00:38 +0800

    Класс Поле.

---
+++ CatMouse/models/Field.php
@@ -0,0 +1,31 @@
+<?php
+namespace Application\CatMouse\models;
+
+
+use Application\CatMouse\classes\Animal;
+use \SplObjectStorage;
+
+class Field {
+    private $size;
+    private $listOfAnimals;
+
+    public function __construct($size){
+        $this->size = $size;
+        $this->listOfAnimals = new SplObjectStorage();
+    }
+
+    public function addAnimalToField(Animal $animal){
+        $this->listOfAnimals->attach($animal);
+    }
+
+    public function deleteAnimalFromField(Animal $animal){
+        if($this->getListOfAnimals()->contains($animal)){
+            $this->getListOfAnimals()->detach($animal);
+        }
+    }
+
+    public function getListOfAnimals()
+    {
+        return $this->listOfAnimals;
+    }
+}

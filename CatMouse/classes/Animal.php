61611277afed217f2df6e60c862713a520ca2ec4
Integer64 <tivan@bk.ru>
2015-07-11 00:43:30 +0800

    Base class animal.

--- CatMouse/classes/Animal.php
+++ CatMouse/classes/Animal.php
@@ -2,9 +2,11 @@
 namespace Application\CatMouse\classes;
 
 abstract class Animal{
+    CONST RANDFORWALK = 1;
     private $fieldOfVision;
     private $cruisingRange;
-    private $location = [0, 0];
+    private $location = ["x" => 0, "y" => 0];
+    protected $seeAnimals;
 
     public function __construct($fieldOfVision, $cruisingRange){
         $this->fieldOfVision = $fieldOfVision;
@@ -41,6 +43,22 @@ abstract class Animal{
         $this->location = $location;
     }
 
+    protected function getRangeView(){
+        $location = $this->getLocation();
+        $cruisingRange = $this->getCruisingRange();
+        $xMin = $location["x"] - $cruisingRange < 0 ? 0 : $location["x"] - $cruisingRange;
+        $xMax = $location["x"] + $cruisingRange;
+        $xRange = ["xMin" => $xMin, "xMax" => $xMax];
 
+        $yMin = $location["y"] - $cruisingRange < 0 ? 0 : $location["y"] - $cruisingRange;
+        $yMax = $location["y"] + $cruisingRange;
+        $yRange = ["yMin" => $yMin, "yMax" => $yMax];
+
+        return ["x" => $xRange, "y" => $yRange];
+    }
+
+    abstract public function getLabel();
+
+    abstract public function walk();
 
 } 

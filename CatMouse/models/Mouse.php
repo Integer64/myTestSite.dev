557afe0c645b0e18da94b177bed36c885a37864b
Integer64 <tivan@bk.ru>
2015-07-11 03:05:14 +0800

    Add check to walk random

--- CatMouse/models/Mouse.php
+++ CatMouse/models/Mouse.php
@@ -2,16 +2,20 @@
 namespace Application\CatMouse\models;
 
 use Application\CatMouse\classes\Animal;
-use \SplObjectStorage;
 
-class Mouse extends Animal{
 
-    public function walk(){
+class Mouse extends Animal
+{
+
+    public function walk()
+    {
         $location = $this->getLocation();
         $cruisingRange = $this->getCruisingRange();
 
         $X = $location["x"];
         $Y = $location["y"];
+
+        if ($this->seeAnimals->count() === 0) {
 
             $xRand = mt_rand(-self::RANDFORWALK, self::RANDFORWALK);
             $yRand = mt_rand(-self::RANDFORWALK, self::RANDFORWALK);
@@ -29,26 +33,14 @@ class Mouse extends Animal{
             } elseif ($yRand < 0) {
                 $Y = $location["y"] - $cruisingRange < 0 ? 0 : $location["y"] - $cruisingRange;
             }
-
-        $this->setLocation(["x" => $X, "y" =>$Y]);
-    }
-
-    public function lookAround(SplObjectStorage $animals){
-        $this->seeAnimals = new SplObjectStorage();
-        $range = $this->getRangeView();
-        foreach ($animals as $animal) {
-            if($animal === $this) continue;
-            if($animal instanceof Cat){
-                $animalLocation = $animal->getLocation();
-                if(($animalLocation["x"] >= $range["x"]["xMin"] || $animalLocation["x"] <= $range["x"]["xMax"])
-                    && ($animalLocation["y"] >= $range["y"]["yMin"] || $animalLocation["y"] <= $range["y"]["yMax"])){
-                    $this->seeAnimals->attach($animal);
-                }
-            }
         }
+
+        $this->setLocation(["x" => $X, "y" => $Y]);
     }
 
-    public function getLabel(){
+
+    public function getLabel()
+    {
         return "M";
     }
 

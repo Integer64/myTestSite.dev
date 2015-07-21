01a38862cfd97d86304f90fab5e3837138111266
Integer64 <tivan@bk.ru>
2015-07-21 21:08:34 +0800

    Добавил констант для выбора направления. Также добавил свойства с врагами и дружелюбными животнами. Изменил конструктор для св-ва fieldVision. Переделал метод walk().

--- CatMouse/models/Mouse.php
+++ CatMouse/models/Mouse.php
@@ -3,41 +3,168 @@ namespace Application\CatMouse\models;
 
 use Application\CatMouse\classes\Animal;
 
-
 class Mouse extends Animal
 {
+    CONST DIRECTION_FIRST = 0;
+    CONST DIRECTION_SECOND = 2;
+
+    private $naturalEnemies = '\Application\CatMouse\models\Cat';
+    private $friendAnimals = '\Application\CatMouse\models\Dog';
+
+    public function __construct($fieldOfVision, $cruisingRange, $name, $fieldSize){
+        parent::__construct($fieldOfVision, $cruisingRange, $name, $fieldSize);
+        $this->setFieldOfVision((int)floor($fieldOfVision / 2));
+    }
 
     public function walk()
     {
+
         $location = $this->getLocation();
-        $cruisingRange = $this->getCruisingRange();
+        $seeAnimals = $this->getSeeAnimals();
+        $naturalEnemies = $this->naturalEnemies;
 
-        $X = $location["x"];
-        $Y = $location["y"];
+        /**
+         *  TODO: Сделать выборку в списке животных в поле зрения, для поиска нужного животного.
+         */
 
-        if ($this->seeAnimals->count() === 0) {
-
-            $xRand = mt_rand(-self::RANDFORWALK, self::RANDFORWALK);
-            $yRand = mt_rand(-self::RANDFORWALK, self::RANDFORWALK);
-
-//            echo "Random x = $xRand, y = $yRand\n";
-
-            if ($xRand > 0) {
-                $X = $location["x"] + $cruisingRange;
-            } elseif ($xRand < 0) {
-                $X = $location["x"] - $cruisingRange < 0 ? 0 : $location["x"] - $cruisingRange;
-            }
-
-            if ($yRand > 0) {
-                $Y = $location["y"] + $cruisingRange;
-            } elseif ($yRand < 0) {
-                $Y = $location["y"] - $cruisingRange < 0 ? 0 : $location["y"] - $cruisingRange;
+        foreach ($seeAnimals as $animal) {
+            if ($animal instanceof $naturalEnemies) {
+                $direction = mt_rand(self::DIRECTION_FIRST, self::DIRECTION_SECOND);
+                $locationEnemies = $animal->getLocation();
+                switch (true) {
+                    // Кот вверху и слева
+                    case ($locationEnemies["x"] < $location["x"] && $locationEnemies["y"] < $location["y"]):
+                        if ($direction > self::DIRECTION_FIRST) {
+                            $this->goRight();
+                        } else {
+                            $this->goDown();
+                        }
+                        echo "Cat is up and left\n";
+                        return;
+                        break;
+                    // Кот вверху и справа
+                    case ($locationEnemies["x"] > $location["x"] && $locationEnemies["y"] < $location["y"]):
+                        if ($direction > self::DIRECTION_FIRST) {
+                            $this->goLeft();
+                        } else {
+                            $this->goDown();
+                        }
+                        echo "Cat is up and right\n";
+                        return;
+                        break;
+                    // Кот внизу и слева
+                    case ($locationEnemies["x"] < $location["x"] && $locationEnemies["y"] > $location["y"]):
+                        if ($direction > self::DIRECTION_FIRST) {
+                            $this->goRight();
+                        } else {
+                            $this->goUp();
+                        }
+                        echo "Cat is down and left\n";
+                        return;
+                        break;
+                    // Кот внизу и справа
+                    case ($locationEnemies["x"] > $location["x"] && $locationEnemies["y"] > $location["y"]):
+                        if ($direction > self::DIRECTION_FIRST) {
+                            $this->goLeft();
+                        } else {
+                            $this->goUp();
+                        }
+                        echo "Cat is down and right\n";
+                        return;
+                        break;
+                    // Кот внизу
+                    case ($locationEnemies["x"] == $location["x"] && $locationEnemies["y"] > $location["y"]):
+                        if($direction == self::DIRECTION_FIRST) {
+                            $this->goUp();
+                        } elseif ($direction == self::DIRECTION_SECOND) {
+                            $this->goLeft();
+                        } else {
+                            $this->goRight();
+                        }
+                        echo "Cat is down\n";
+                        return;
+                        break;
+                    // Кот вверху
+                    case ($locationEnemies["x"] == $location["x"] && $locationEnemies["y"] < $location["y"]):
+                        if($direction == self::DIRECTION_FIRST) {
+                            $this->goDown();
+                        } elseif ($direction == self::DIRECTION_SECOND) {
+                            $this->goLeft();
+                        } else {
+                            $this->goRight();
+                        }
+                        echo "Cat is up\n";
+                        return;
+                        break;
+                    // Кот справо
+                    case ($locationEnemies["x"] > $location["x"] && $locationEnemies["y"] == $location["y"]):
+                        if($direction == self::DIRECTION_FIRST) {
+                            $this->goUp();
+                        } elseif ($direction == self::DIRECTION_SECOND) {
+                            $this->goLeft();
+                        } else {
+                            $this->goDown();
+                        }
+                        echo "Cat is right\n";
+                        return;
+                        break;
+                    // Кот слева
+                    case ($locationEnemies["x"] < $location["x"] && $locationEnemies["y"] == $location["y"]):
+                        if($direction == self::DIRECTION_FIRST) {
+                            $this->goUp();
+                        } elseif ($direction == self::DIRECTION_SECOND) {
+                            $this->goRight();
+                        } else {
+                            $this->goDown();
+                        }
+                        echo "Cat is left\n";
+                        return;
+                        break;
+                    // Не видно кота
+                    default:
+                        echo "Can't see where is cat\n";
+                        break;
+                }
             }
         }
 
-        $this->setLocation(["x" => $X, "y" => $Y]);
+        $this->randomWalk();
     }
 
+    // Выбор случайного направления
+    protected function randomWalk()
+    {
+        // Псевдослучайные числа для выбора направления
+        $xRand = mt_rand(-self::RAND_FOR_WALK, self::RAND_FOR_WALK);
+        $yRand = mt_rand(-self::RAND_FOR_WALK, self::RAND_FOR_WALK);
+
+        switch (true) {
+            // Идем вправо
+            case ($xRand == 1 && $yRand == 0):
+                $this->goRight();
+                return;
+                break;
+            // Идем влево
+            case ($xRand == -1 && $yRand == 0):
+                $this->goLeft();
+                return;
+                break;
+            // Идем вниз
+            case ($xRand == 0 && $yRand == 1):
+                $this->goDown();
+                return;
+                break;
+            // Идем вверх
+            case ($xRand == 0 && $yRand == -1):
+                $this->goUp();
+                return;
+                break;
+            // Стоим
+            default:
+                echo "Stay\n";
+                break;
+        }
+    }
 
     public function getLabel()
     {

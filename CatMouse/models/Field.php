1887e171e6a1b763270f71466ba67f47129f0776
Integer64 <tivan@bk.ru>
2015-07-21 21:09:44 +0800

    Сделал форматирование кода.

--- CatMouse/models/Field.php
+++ CatMouse/models/Field.php
@@ -4,21 +4,25 @@ namespace Application\CatMouse\models;
 use Application\CatMouse\classes\Animal;
 use \SplObjectStorage;
 
-class Field {
+class Field
+{
     private $size;
     private $listOfAnimals;
 
-    public function __construct($size){
+    public function __construct($size)
+    {
         $this->size = $size;
         $this->listOfAnimals = new SplObjectStorage();
     }
 
-    public function addAnimalToField(Animal $animal){
+    public function addAnimalToField(Animal $animal)
+    {
         $this->listOfAnimals->attach($animal);
     }
 
-    public function deleteAnimalFromField(Animal $animal){
-        if($this->getListOfAnimals()->contains($animal)){
+    public function deleteAnimalFromField(Animal $animal)
+    {
+        if ($this->getListOfAnimals()->contains($animal)) {
             $this->getListOfAnimals()->detach($animal);
         }
     }

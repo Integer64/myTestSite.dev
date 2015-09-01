c07c5288491dc1f3afedbf9f4a8165604687d9a5
Integer64 <tivan@bk.ru>
2015-09-02 04:13:38 +0800

    Добавил методы для работы с ячейками.

--- CatMouse/models/Field.php
+++ CatMouse/models/Field.php
@@ -36,4 +36,88 @@ class Field
     {
         return $this->size;
     }
+
+    public function getDistanceToAnimalFromCell(Animal $animal, $cellCoordinates){
+        $location = $cellCoordinates;
+        $locationCell = $animal->getLocation();
+        $distance = sqrt(pow($location["x"] - $locationCell["x"], 2) + pow($location["y"] - $locationCell["y"], 2));
+        return round($distance, 2);
+    }
+
+    public function checkNearCells(Animal $nearAnimal, $checkAnimal){
+        $countOfNearAnimals = 0;
+        $locationMouse = $nearAnimal->getLocation();
+        $coordinatesToCheck = $this->generateCoordinates($locationMouse);
+
+        foreach($coordinatesToCheck as $coordinates){
+            $cell = $this->checkCells($coordinates);
+            if($cell instanceof $checkAnimal){
+                $countOfNearAnimals += 1;
+            }
+        }
+        return $countOfNearAnimals;
+    }
+
+    // Проверить массив на дубликаты
+    private function checkArrayOnDuplicate($array, $checkValue){
+        foreach($array as $value){
+            $arr = array_diff_assoc($value, $checkValue);
+            if(count($arr) == 0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function generateCoordinates($center)
+    {
+        $coordinatesToCheck = [];
+
+        $coordinates[] = ["x" => $center["x"] - 1 < 1 ? $center["x"] : $center["x"] - 1,
+            "y" => $center["y"] - 1 < 1 ? $center["y"] : $center["y"] - 1];
+
+        $coordinates[] = ["x" => $center["x"] - 1 < 1 ? $center["x"] : $center["x"] - 1,
+            "y" => $center["y"]];
+
+        $coordinates[] = ["x" => $center["x"] - 1 < 1 ? $center["x"] : $center["x"] - 1,
+            "y" => $center["y"] + 1 > $this->getSize() ? $center["y"] : $center["y"] + 1];
+
+        $coordinates[] = ["x" => $center["x"],
+            "y" => $center["y"] + 1 > $this->getSize() ? $center["y"] : $center["y"] + 1];
+
+        $coordinates[] = ["x" => $center["x"] + 1 > $this->getSize() ? $center["x"] : $center["x"] + 1,
+            "y" => $center["y"] + 1 > $this->getSize() ? $center["y"] : $center["y"] + 1];
+
+        $coordinates[] = ["x" => $center["x"] + 1 > $this->getSize() ? $center["x"] : $center["x"] + 1,
+            "y" => $center["y"]];
+
+        $coordinates[] = ["x" => $center["x"] + 1 > $this->getSize() ? $center["x"] : $center["x"] + 1,
+            "y" => $center["y"] - 1 < 1 ? $center["y"] : $center["y"] - 1];
+
+        $coordinates[] = ["x" => $center["x"],
+            "y" => $center["y"] - 1 < 1 ? $center["y"] : $center["y"] - 1];
+
+        foreach ($coordinates as $coordinate) {
+            if (count(array_diff_assoc($center, $coordinate)) != 0) {
+                $isDuplicate = $this->checkArrayOnDuplicate($coordinatesToCheck, $coordinate);
+                if ($isDuplicate == false) {
+                    $coordinatesToCheck[] = $coordinate;
+                }
+            }
+        }
+
+        return $coordinatesToCheck;
+    }
+
+    // Проверить ячейку
+    public function checkCells($coordinatesCell){
+        $list = $this->getListOfAnimals();
+        foreach ($list as $animal){
+            $coordinates = $animal->getLocation();
+            if($coordinates["x"] == $coordinatesCell["x"] && $coordinates["y"] == $coordinatesCell["y"]){
+                return $animal;
+            }
+        }
+        return null;
+    }
 }
